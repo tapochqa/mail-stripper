1403c22f87010b675123bbf921ed85fd18975a87
Maxim Sakovich <max@limonadny.ru>
2026-06-29 09:49:39 +0300

    - test

--- test/mail_stripper/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns mail-stripper.core-test
-  (:require [clojure.test :refer :all]
-            [mail-stripper.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
